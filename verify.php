f596ba79e9170720fd8ae5b973666ba22f1bb492
suparin13 <152370501+suparin13@users.noreply.github.com>
2024-01-03 15:49:44 +0700

    Work4
    Work4

---
+++ verify.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard SPR</h1>
+    <hr>
+    <div style="text-align: center;">
+        เข้าสู่ระบบด้วย<br>
+        Login = <?php echo $_POST['login']; ?><br>
+        Password = <?php echo $_POST['pwd']; ?> 
+    </div>
+</body>
+</html>
