f25598ec3fcb2dc3276946433d7729b02bd097db
phutadolza <phutadol5627@gmail.com>
2024-02-28 16:56:19 +0700

    Update webboard.sql

--- webboard.sql
+++ webboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2024 at 10:47 AM
+-- Generation Time: Feb 28, 2024 at 10:54 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -31,6 +31,15 @@ CREATE TABLE `category` (
   `id` int(11) NOT NULL,
   `name` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'เรื่องทั่วไป'),
+(2, 'เรื่องเรียน'),
+(3, 'เรื่องกีฬา');
 
 -- --------------------------------------------------------
 
@@ -61,6 +70,15 @@ CREATE TABLE `post` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`id`, `title`, `content`, `post_date`, `cat_id`, `user_id`) VALUES
+(1, 'บลาๆๆๆๆ', 'fgfgfh', '2024-02-28 16:41:51', 1, 10),
+(2, 'จะFมั้ยคับ', 'กลัวจะFแน่นอน', '2024-02-28 16:43:32', 2, 9),
+(3, 'ทำยังไงให้เพื่อนไปตีแบตด้วยคะ', 'เพื่อนไม่ยอมไปตีแบตด้วยค่ะเพื่อนบอกไม่ชอบเราจะทำยังไงให้เพื่อนชอบคะ', '2024-02-28 16:47:35', 3, 9);
+
 -- --------------------------------------------------------
 
 --
@@ -76,6 +94,20 @@ CREATE TABLE `user` (
   `email` varchar(32) NOT NULL,
   `role` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `login`, `password`, `name`, `gender`, `email`, `role`) VALUES
+(1, '555', '5555', 'สุภรินทร์ ศรีคล้อย', 'f', 'suparin@kmutnb.ac.th', 'm'),
+(2, 'SPR', '111', 'nat spr', 'f', 'spr@gmail.com', 'm'),
+(3, 'hamgee', '1314', 'jm js', 'o', 'js1314@email.com', 'm'),
+(4, 'ham', '1314', 'jm js', 'o', 'js131405@email.com', 'm'),
+(8, 'suparin', '3010', 'suparin srikhloi', 'o', 'suparin3010@email.com', 'm'),
+(9, 'admin', '8dc9fa69ec51046b4472bb512e292d959edd2aef', 'admin', 'o', 'admin@email.com', 'a'),
+(10, 'fff', 'f6949a8c7d5b90b4a698660bbfb9431503fbb995', 'fff', 'o', 'fff@gmail.com', 'm'),
+(11, 'member', 'b54df48c4c77522382a5a3c2f0358573ad43746e', 'member', 'o', 'member@gmail.com', 'm');
 
 --
 -- Indexes for dumped tables
@@ -113,7 +145,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `comment`
@@ -125,13 +157,13 @@ ALTER TABLE `comment`
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
