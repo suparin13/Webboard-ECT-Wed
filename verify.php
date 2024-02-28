f7a9aeb19d0cd1b4c250b4db345f1b8e36734411
phutadolza <phutadol5627@gmail.com>
2024-02-28 16:54:56 +0700

    work9
    work9

--- verify.php
+++ verify.php
@@ -13,32 +13,31 @@ if(isset($_SESSION['id'])){
     <title>Verify</title>
 </head>
 <body>
-
     <?php
-        $login = $_POST['login'];
-        $pwd = $_POST['pwd'];
-        if($login == "admin" && $pwd == "ad1234"){
-            $_SESSION["username"] = "admin";
-            $_SESSION["role"] = "a";
-            $_SESSION["id"] = session_id();
+        session_start();
+        if(isset($_SESSION['id'])){
             header("location:index.php");
             die();
-            //echo "ยินดีต้อนรับคุณ ADMIN";
-        } 
-        else if($login == "member" && $pwd == "mem1234"){
-            $_SESSION["username"] = "member";
-            $_SESSION["role"] = "m";
-            $_SESSION["id"] = session_id();
+        }
+        $login=$_POST['login'];
+        $pwd=$_POST['pwd'];
+        $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+        $sql="SELECT * FROM user where login='$login' and password=sha1('$pwd')";
+        $result=$conn->query($sql);
+        if($result->rowCount()==1){
+            $data=$result->fetch(PDO::FETCH_ASSOC);
+            $_SESSION['username']=$data['login'];
+            $_SESSION['role']=$data['role'];
+            $_SESSION['user_id']=$data['id'];
+            $_SESSION['id']=session_id();
             header("location:index.php");
             die();
-            //echo "ยินดีต้อนรับคุณ MEMBER";
-        } 
-        else{
-            $_SESSION['error'] = 'error';
+        }else{
+            $_SESSION['error']="error";
             header("location:login.php");
             die();
-            //echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
         }
+        $conn=null;
     ?>
 </body>
 </html>
