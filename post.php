b29b103129de0458ec63994b1b14be14200f1132
phutadolza <phutadol5627@gmail.com>
2024-03-06 17:01:44 +0700

    work10 post.php post_save.php
    work10 post.php post_save.php

--- post.php
+++ post.php
@@ -1,32 +1,82 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Post</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard SPR</h1>
-    <hr>
-    <div style="text-align: center;">
-    <?php
-       $num = $_GET['id'];
-       echo "ต้องการดูกระทู้หมายเลข $_GET[id]<br>";
-       if($num %2 == 0){
-        echo "เป็นกระทู้หมายเลขคู่";
-    }
-    else{
-        echo "เป็นกระทู้หมายเลขคี่";
-    } 
-    ?>
+    <div class="container-lg">
+        <h1 style="text-align: center;" class="mt-3">Webboard SPR</h1>
+        <?php include "nav.php" ?>
+        <div class="row mt-4">
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+            <div class="col-lg-6 col-md-8 col-sm-10">
+            <?php 
+                $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+                $sql = "select post.title,post.content,post.post_date,user.login 
+                from post inner join user on (post.user_id=user.id) where post.id=$_GET[id]";
+                $result = $conn->query($sql);
+                while($row=$result->fetch()){
+                    echo "<div class='card border-primary'>";
+                    echo "<div class='card-header bg-primary text-white'>$row[0]</div>";
+                    echo "<div class='card-body'>$row[1]<br>$row[3] - $row[2]</div>";
+                    echo "</div>";
+
+                }
+            ?>
+            <br>
+            <?php 
+                $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+                $sql = "select comment.content,comment.post_date,user.login 
+                from comment inner join user on (comment.user_id=user.id) where comment.post_id=$_GET[id]";
+                $result = $conn->query($sql);
+                $i = 1;
+                while($row=$result->fetch()){
+                    echo "<div class='card border-info'>";
+                    echo "<div class='card-header bg-info text-white'>ความคิดเห็นที่ $i</div>";
+                    echo "<div class='card-body'>$row[0]<br>$row[2] - $row[1]</div>";
+                    echo "</div><br>";
+                    $i = $i+1;
+                }
+                $conn=null;
+            ?>
+                <?php if(isset($_SESSION['id'])){ ?>
+                <div class="card border-success mt-3">
+                    <div class="card-header bg-success text-white">
+                        แสดงความคิดเห็น</div>
+                <div class="card-body">
+                    <form action="post_save.php" method="post">
+                        <input type="hidden" name="post_id" value="<?= $_GET['id'];?>">
+                        <div class="row mb-3 justify-content-center">
+                            <div class="col-lg-10">
+                                <textarea name="comment" rows="8" class="form-control" require></textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 d-flex justify-content-center">
+                                <button type="submit" class="btn btn-success btn-sm text-white">
+                                    <i class="bi bi-box-arrow-up-right"></i>ส่งข้อความ
+                                </button>
+                                <button type="reset" class="btn btn-danger btn-sm ms-2">
+                                <i class="bi bi-x-square"></i> ยกเลิก</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <?php } ?>
+            <br>
+            </div>
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+        </div>
     </div>
-    <br>
-        <table style="border: 2px solid black; width: 40%;" align="center">
-            <tr><td colspan="2" style="background-color: #6CD2FE;">แสดงความคิดเห็น</td></tr>
-            <tr><td><textarea rows="10" cols = "70"></textarea></td></tr>
-            <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
-        </table>
-    <br>
     <div style="text-align: center;"><a href="index.php">กลับไปหน้าหลัก</a></div>
 </body>
 </html>
