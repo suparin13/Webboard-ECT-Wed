3b507d3cad288a3605235b3c0d6f89972ccbff11
phutadolza <phutadol5627@gmail.com>
2024-03-13 16:41:54 +0700

    Update webboard.sql

--- webboard.sql
+++ webboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2024 at 10:39 AM
+-- Generation Time: Mar 13, 2024 at 10:41 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
